b62fa90372ae1f14e63517a5be023edc7e06f74c
jeffersonamado <jeffersonamado@gmail.com>
2024-06-21 18:40:29 -0500

    refactor(front): add frontend

--- backend/src/server.py
+++ backend/src/server.py
@@ -19,7 +19,7 @@ def tokenize(alg):
   }[alg]
   return jsonify({"text": text, "algorithm":alg, "tokenized":result})
 
-@app.route("/stemmeing/<int:alg>/tokenize/<int:tk>", methods=['POST'])
+@app.route("/stemming/<int:alg>/tokenize/<int:tk>", methods=['POST'])
 def stemmeing(alg, tk):
   text = request.json['text']
   result = {
@@ -27,7 +27,7 @@ def stemmeing(alg, tk):
     2: model2.tokenize(text),
     3: model3.tokenize(text),
   }[tk]
-  stemmed = [ [model4.stem(a_token) for a_token in a_sentence] for a_sentence in result]
+  stemmed = [model4.stem(a_token) for a_token in result]
   return jsonify({"text": text, "algorithm":alg, "tokenized":result, "stemmed": stemmed})
 
 @app.route("/lemmatizer/<int:alg>/tokenize/<int:tk>", methods=['POST'])
@@ -39,7 +39,7 @@ def lemmatizer(alg, tk):
     3: model3.tokenize(text),
   }[tk]
 
-  lemmatized = [ [model5.lemmatize(a_token) for a_token in a_sentence] for a_sentence in result]
+  lemmatized = [model5.lemmatize(a_token) for a_token in result]
   return jsonify({"text": text, "algorithm":alg, "tokenized":result, "lemmatized": lemmatized})
 
 
