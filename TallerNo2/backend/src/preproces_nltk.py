4878d1db53ef48e4df87c2ce1dbf42628851722d
jeffersonamado <jeffersonamado@gmail.com>
2024-06-22 10:16:59 -0500

    feat(t2): add preprocess nltk

--- TallerNo2/backend/src/preproces_nltk.py
+++ TallerNo2/backend/src/preproces_nltk.py
@@ -19,19 +19,29 @@ nltk.download("omw-1.4")
 stemmer = SnowballStemmer('spanish')
 lemmatizer = WordNetLemmatizer()
 
-def preprocessing(paragraphs:str, do_stem:bool=False, do_post:bool=False) ->list:
+def preprocessing(paragraphs:str, do_stem:bool=False, do_post:bool=False) ->dict:
   # Segmentation -- Cuando es un parrafo debo separar las oraciones
   lsentences = sent_tokenize(paragraphs)
   # Tokenization  
   lltokens = [word_tokenize(a_sentence) for a_sentence in lsentences]
   ## Stemming (Optional)
   if do_stem: 
-    stemmed = [[stemmer(a_token) for a_token in a_sentence] for a_sentence in lltokens]
+    stemmed = [[stemmer.stem(a_token) for a_token in a_sentence] for a_sentence in lltokens]
   ## Postagging (Optional)
   if do_post:
     tagged = [post_tag(a_sentence) for a_sentence in lltokens]
   # Lemmatization
-
+  lemmatized = [[lemmatizer.lemmatize(a_token) for a_token in a_sentence] for a_sentence in lltokens]
   # Remove Stop words
   # Remove punctuation
 
+
+## testing preprocessing
+a_paragraph = "Un párrafo es una unidad de un texto compuesta por una o varias oraciones"\
+  ", que comienza con una mayúscula y que termina con un punto y aparte. Los textos se "\
+  "organizan de manera tal que cada párrafo trata sobre una idea central. "\
+  "Generalmente, la primera oración de cada párrafo suele explicitar cuál es el "\
+  "punto principal que se desarrollará."
+
+result= preprocessing(a_paragraph)
+print(result)
