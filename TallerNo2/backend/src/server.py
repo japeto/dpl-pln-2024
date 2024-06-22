97c6ff4f5d3d25552c8f5e92911070f24bbeb5a3
jeffersonamado <jeffersonamado@gmail.com>
2024-06-22 11:59:23 -0500

    feat(t2): add server

--- TallerNo2/backend/src/server.py
+++ TallerNo2/backend/src/server.py
@@ -3,3 +3,18 @@ from flask_cors import CORS
 
 from preproces_nltk import preprocessing as pnltk
 from preproces_spacy import preprocessing as pspacy
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/ping", methods=['GET'])
+def pong():
+  return jsonify({"message":"pong"})
+
+@app.route("/preprocessing/<method>", methods=['POST'])
+def preprocessing(method):
+  text= request.json['text']
+  return jsonify({
+    1:pnltk(text),
+    2:pspacy(text)
+  }[method])
